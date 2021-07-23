19dd151e5529726194ee3ddbbea9e2f6ab64e176
Muhammad Nuriman bin Roslan <nuriman.angkara.nakhoda.tech@gmail.com>
2021-07-24 00:20:01 +0800

    appconfig

--- src/app/app.service.ts
+++ src/app/app.service.ts
@@ -7,3 +7,10 @@ export class AppService {
 
   constructor() { }
 }
+
+export const AppConfig ={
+  
+  // api:"https://tm-mobile-apps-test.000webhostapp.com/tm-test/"
+  api:"http://192.168.1.7/tm-test/"
+
+}
